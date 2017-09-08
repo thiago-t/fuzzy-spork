4dddc69c3b44f8d4956c556397005a6e5bfd83fe
Thiago Toazza <thiagotoazza@outlook.com>
2017-09-08 15:54:19 -0300

    Comentários e mensagens de exceção

--- sum/src/test/java/s2b/tst/sum/Sum.java
+++ sum/src/test/java/s2b/tst/sum/Sum.java
@@ -4,10 +4,10 @@ public class Sum {
 
 	public static double sum(double d, double e) {
 		if (d < 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("O primeiro número não deve ser negativo!");
 		}
 		if (e < 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("O segundo número não deve ser negativo!");
 		}
 		return d + e;
 	}
