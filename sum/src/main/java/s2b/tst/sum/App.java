a976cebcdab7bdaf63835db3176caa33e55d6d5b
Thiago Toazza <thiagotoazza@outlook.com>
2017-09-12 15:13:26 -0300

    #1

--- sum/src/main/java/s2b/tst/sum/App.java
+++ sum/src/main/java/s2b/tst/sum/App.java
@@ -1,13 +1,29 @@
 package s2b.tst.sum;
 
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	private static Scanner num;
+
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		Scanner input = new Scanner(System.in);
+		num = new Scanner(System.in);
+		DecimalFormat deci = new DecimalFormat("0.00");
+		System.out.printf("Enter A value:\n");
+		double a = num.nextDouble();
+		System.out.printf("Enter B value:\n");
+		double b = num.nextDouble();
+		input.close();
+		double r = Sum.sum(a, b);
+		System.out.printf("A= %f\n", a);
+		System.out.printf("B= %f\n", b);
+		System.out.println(deci.format(r));
     }
 }
